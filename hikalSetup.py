cbcb5ef98b65805dde2df46b4570db08793414ad
Okano Yoshihisa <okano@win.nd.co.jp>
2019-02-13 17:42:30 +0900

    hikal export

---
+++ hikalSetup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+assetHikal = 'P:/Project/mem2/assets/chara/DDHikal/DDHikalNoCloth/publish/Model/RenderHigh/maya/current/DDHikalNoCloth_mdlRH.mb'
+assetHair = 'P:/Project/mem2/assets/chara/DDHikal/DDHikalNoCloth/publish/setup/RenderHighHair/maya/current/DDHikalNoCloth_RigRHhair.mb'
+nsHikal = 'DDHikalNml'
+nsHair = 'DDHikalNmlHair'
+topNode = 'DDHikalNml:DDHikalNml'
