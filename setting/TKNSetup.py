d28bf5ffe05f915fce9086fd4045277a46f84a67
Okano Yoshihisa <okano@win.nd.co.jp>
2019-04-22 11:16:52 +0900

    TKN2_A_bodyBrokenに一致するもののみをエクスポートするように変更

--- setting/TKNSetup.py
+++ setting/TKNSetup.py
@@ -2,7 +2,7 @@
 
 assetChara = 'P:/Project/mem2/assets/chara/TKN2R/TKN2_A_bodyBroken/publish/Setup/RigRH/maya/current/TKN2_A_bodyBroken_RigRH.mb'
 # assetHair = ''
-nsChara = ['[a-zA-Z0-9_:]*TKN2_A_bodyBroken[a-zA-Z0-9_]*', '[a-zA-Z0-9_:]*TKN2_A[1-9]*']
+nsChara = ['[a-zA-Z0-9_:]*TKN2_A_bodyBroken[a-zA-Z0-9_]*']
 # nsHair = ''
 # topNode = 'geo_GP'
 # abcSet = ['ABCset']
