809c4d8c50ee71b99d143fed046b876aa26a1792
Okano Yoshihisa <okano@win.nd.co.jp>
2019-06-17 12:30:02 +0900

    regex update

--- setting/kesedoASetup.py
+++ setting/kesedoASetup.py
@@ -9,4 +9,7 @@ nsChara = ['CHkesedoA[a-zA-Z0-9_:]*']
 # abcSet = ['ABCset']
 regex = ["*_ctrl_L","*_ctrl_R","*_ctrl_FL","*_ctrl_OL","*_ctrl_BL","*_ctrl_IL","*_ctrl_FR","*_ctrl_OR","*_ctrl_BR", "*_ctrl_IR", "*_ctrl_B", "*_ctrl",
          "*_ctrl_UL", "*_ctrl_DL", "*_ctrl_UR", "*_ctrl_DR", "*_ctrl_ZipL", "*_ctrl_ZipR", "*_ctrl_C", "*_ctrl_OL", "*_ctrl_OR", "*_ctrl_U", "*_ctrl_D", "*_ctrl_Zip",
-         "*_ctrl_UC", "*_ctrl_DC", "*_ctrl_ZipC", "*_ctrl_AL", "*_ctrl_BL", "*_ctrl_CL", "*_ctrl_AR", "*_ctrl_BR", "*_ctrl_CR", "*_ctrl_TL", "*_ctrl_TR"]
+         "*_ctrl_UC", "*_ctrl_DC", "*_ctrl_ZipC", "*_ctrl_AL", "*_ctrl_BL", "*_ctrl_CL", "*_ctrl_AR", "*_ctrl_BR", "*_ctrl_CR", "*_ctrl_TL", "*_ctrl_TR",
+         "*_fkCtrl_C", "*_ikCtrl_R", "*_ikCtrl", "*_fkCtrl_L", "*_fkCtrl", "*_fkCtrl_R", "*_ikCtrl_L", "*_ikCtrl_C",
+         "*_twk_R", "*_twk_FR", "*_twk_BR", "*_ctrl_C1", "*_twk_BC", "*_twk_FC", "*_ctrl_FC", "*_twk_BL", "*_ctrl_F", "*_ctrl_BC", "*_twk_FL", "*_twk_L", 
+         "*_ctrl_R1", "*_ctrl_L1"]
