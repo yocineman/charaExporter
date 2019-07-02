c5c07902270b9470955e75c8a8e6f1cf814d9d64
Okano Yoshihisa <okano@win.nd.co.jp>
2019-07-02 15:43:48 +0900

    misc

--- setting/DCT_CescapeSetup.py
+++ setting/DCT_CescapeSetup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-assetChara = 'P:/Project/mem2/assets/bg/DCT_C/DCT_Cescape/publish/model/RenderHigh2/maya/current/DCT_Cescape_mdl.mb'
+assetChara = 'P:/Project/mem2/assets/bg/DCT_C/DCT_Cescape/publish/model/MdlRH2/maya/current/DCT_Cescape_MdlRH2.mb'
 
 nsChara = ['[a-zA-Z0-9_:]*DCT_Cescape[a-zA-Z0-9_]*']
 
---
+++ setting/LXMSetup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+assetChara = 'P:/Project/mem2/assets/chara/FRD/001_LXM/publish/setup/RigRH/maya/current/001_LXM_RigRH.mb'
+assetHair = ''
+nsChara = ['[a-zA-Z0-9_:]*_LXM$']
+nsHair = ''
+topNode = 'geo_GP'
+abcSet = ['ABCset']
