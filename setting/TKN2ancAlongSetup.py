fc20f62606bdef13fc5ea3eefcab6983a2182d12
Okano Yoshihisa <okano@win.nd.co.jp>
2019-07-16 16:46:40 +0900

    0.7.1 DCT_Cアセット追加

---
+++ setting/TKN2ancAlongSetup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+assetChara = 'P:/Project/mem2/assets/chara/TKN2R/TKN2ancAlong/publish/Setup/RigRH/maya/current/TKN2ancAlong_RigRH.mb'
+# assetHair = ''
+nsChara = ['[a-zA-Z0-9_:]*TKN2ancAlong$']
+# nsHair = ''
+topNode = 'TKN2ancA'
+# abcSet = ['ABCset']
+regex = ["*_ctrl"]
