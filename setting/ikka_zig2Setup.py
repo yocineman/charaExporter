c976d4f57ca18d0cd6ab41bad0d86c1cbb33c193
Okano Yoshihisa <okano@win.nd.co.jp>
2019-08-01 12:27:51 +0900

    add settings

---
+++ setting/ikka_zig2Setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+assetChara = 'P:/Project/mem2/assets/chara/DDIkka/DDIkkaNml/publish/setup/RigRH10/maya/current/DDIkkaNml_RigRH10.mb'
+assetHair = ''
+nsChara = ['[a-zA-Z0-9_:]*DDIkkaNml']
+nsHair = ''
+topNode = 'geo_GP'
+abcSet = ['ABCset']
---
+++ setting/naoto_zig2Setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+assetChara = 'P:/Project/mem2/assets/chara/DDNaoto/DDNaotoNml/publish/setup/RigRH10/maya/current'
+assetHair = ''
+nsChara = ['[a-zA-Z0-9_:]*DDNaotoNml']
+nsHair = ''
+topNode = 'geo_GP'
+abcSet = ['ABCset']
